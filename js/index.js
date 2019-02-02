4437d8e79499533b7acfd3371f3aa69653f39bb5
MAbbas <mabbashm110@gmail.com>
2019-02-02 11:50:26 -0600

    MVP done without stretch

--- js/index.js
+++ js/index.js
@@ -37,6 +37,99 @@ const siteContent = {
   },
 };
 
+
+// TASK 2 & 3:
+//Standard function to create elements
+
+// 
+function createIndexElement(elementProp) {
+  
+  for (let i=0; i < elementProp.length; i++){
+    // console.log(elementProp[i].type);
+    let newElement = document.createElement(elementProp[i].type);
+    if (elementProp[i].attrib !== ""){
+      // console.log("Not done")
+      newElement.setAttribute(elementProp[i].attrib, elementProp[i].attribInfo);
+    }
+    if (elementProp[i].className != ""){
+      newElement.classList.add(elementProp[i].className);
+    }
+    newElement.textContent = elementProp[i].textContent;
+    const newPlacement = document.querySelector(elementProp[i].parent);
+    // console.log(newElement);
+    // const newPlacement = document.querySelectorAll(elementProp[i].parent);
+    // if (newPlacement.length > 1) {
+      // newPlacement.forEach(item => item.appendChild(newElement));
+    // }
+    // else {
+
+    // TASK 4: Define whether it is an append or a prepend - reference used in the elements definition
+    if (elementProp[i].method == "append" || elementProp[i].method == "") {
+      newPlacement.appendChild(newElement);
+    }
+    else {
+      newPlacement.prepend(newElement);
+    }
+      
+    // }
+    
+    console.log(newPlacement);
+    // newPlacement.appendChild(newElement); 
+    // console.log(newElement);
+  }
+}
+
+// NAV and CTA
+let elements = [
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": siteContent["nav"]["nav-item-1"], "parent": "nav", "method": "append"},
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": siteContent["nav"]["nav-item-2"], "parent": "nav", "method": "append"},
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": siteContent["nav"]["nav-item-3"], "parent": "nav", "method": "append"},
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": siteContent["nav"]["nav-item-4"], "parent": "nav", "method": "append"},
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": siteContent["nav"]["nav-item-5"], "parent": "nav", "method": "append"},
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": "NAV6", "parent": "nav", "method": "append"},
+  {"type": "a", "attrib": "href", "attribInfo": "#", "textContent": "NAV5", "parent": "nav", "method": "prepend"},
+  {"type": "h1", "attrib": "", "attribInfo": "", "textContent": siteContent["cta"]["h1"], "parent": ".cta-text", "method": "append"},
+  {"type": "button", "attrib": "", "attribInfo": "", "textContent": siteContent["cta"]["button"], "parent": ".cta-text", "method": "append"}
+  // {"type": "div", "attrib": "", "attribInfo": "", "textContent": siteContent["main-content"]["features-h4"], "parent": "top-content", "className": "text-content"},
+  // {"type": "h4", "attrib": "", "attribInfo": "", "textContent": siteContent["main-content"]["features-h4"], "parent": ".top-content"},
+  // {"type": "p", "attrib": "", "attribInfo": "", "textContent": siteContent["main-content"]["features-content"], "parent": "#features"},
+  // {"type": "h4", "attrib": "", "attribInfo": "", "textContent": siteContent["main-content"]["about-h4"], "parent": "#about"},
+  // {"type": "p", "attrib": "", "attribInfo": "", "textContent": siteContent["main-content"]["about-content"], "parent": "#about"},
+  // {"type": "h1", "attrib": "", "attribInfo": "", "textContent": siteContent["cta"]["h1"], "parent": ".cta-text"},
+  // {"type": "h1", "attrib": "", "attribInfo": "", "textContent": siteContent["cta"]["h1"], "parent": ".cta-text"},
+  // {"type": "h1", "attrib": "", "attribInfo": "", "textContent": siteContent["cta"]["h1"], "parent": ".cta-text"},
+  // {"type": "h1", "attrib": "", "attribInfo": "", "textContent": siteContent["cta"]["h1"], "parent": ".cta-text"},
+];
+
+createIndexElement(elements);
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.querySelector("#cta-img");
+ctaImg.src = siteContent["cta"]["img-src"];
+
+let midImg = document.getElementsByClassName("middle-img");
+midImg[0].src = siteContent["main-content"]["middle-img-src"];
+
+
+let textConHeaders = document.querySelectorAll(".text-content h4");
+//top content h4
+textConHeaders[0].innerHTML = siteContent["main-content"]["features-h4"]
+textConHeaders[1].innerHTML = siteContent["main-content"]["about-h4"]
+//bottom content h4
+textConHeaders[2].innerHTML = siteContent["main-content"]["services-h4"];
+textConHeaders[3].innerHTML = siteContent["main-content"]["product-h4"];
+textConHeaders[4].innerHTML = siteContent["main-content"]["vision-h4"];
+
+//top content para
+let textConPara = document.querySelectorAll(".text-content p");
+textConPara[0].innerHTML = siteContent["main-content"]["features-content"];
+textConPara[1].innerHTML = siteContent["main-content"]["about-content"];
+//bottom content para
+textConPara[2].innerHTML = siteContent["main-content"]["services-content"];
+textConPara[3].innerHTML = siteContent["main-content"]["product-content"];
+textConPara[4].innerHTML = siteContent["main-content"]["vision-content"];
+
+
